014a7f3a6c9df9b3729d8f0151fb40ccbc15ad90
Mihail Cornescu <cornescu_mihail@yahoo.com>
2021-11-17 14:39:29 +0200

    first commit

---
+++ src/main/java/com/mvpmatch/vendingmachine/auth/ApplicationUserDao.java
@@ -0,0 +1,9 @@
+package com.mvpmatch.vendingmachine.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+
+}
